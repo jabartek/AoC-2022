55cc2ed7a1162faa643d4ee0d3d85baaafe2db9b
Bartlomiej Janowski <jabartek@hotmail.com>
2022-12-01 18:11:20 +0100

    Day 1 - Removed duplicate code

--- tasks/day_1/src/main.cc
+++ tasks/day_1/src/main.cc
@@ -8,18 +8,20 @@
 
 using namespace jabartek::aoc;
 
-int task_1(utilities::FileLoader &file_loader);
-int task_2(utilities::FileLoader &file_loader);
+std::vector<int> carriedSums(utilities::FileLoader &file_loader);
+int task_1(const std::vector<int> &sums);
+int task_2(std::vector<int> &sums);
 
 int main(int argc, char **argv) {
   const auto args = utilities::to_args({argc, argv});
   auto file_loader = utilities::FileLoader(args.args[0]);
+  auto sums = carriedSums(file_loader);
 
-  std::cout << "Task 1.1:\t" << task_1(file_loader) << "\n";
-  std::cout << "Task 1.2:\t" << task_2(file_loader) << "\n";
+  std::cout << "Task 1.1:\t" << task_1(sums) << "\n";
+  std::cout << "Task 1.2:\t" << task_2(sums) << "\n";
 }
 
-int task_1(utilities::FileLoader &file_loader) {
+std::vector<int> carriedSums(utilities::FileLoader &file_loader) {
   const auto data = file_loader.as_optional<int>();
   std::vector<int> sums;
   int local = 0;
@@ -34,24 +36,12 @@ int task_1(utilities::FileLoader &file_loader) {
   if (local) {
     sums.push_back(local);
   }
-  return std::ranges::max(sums);
+  return sums;
 }
 
-int task_2(utilities::FileLoader &file_loader) {
-  const auto data = file_loader.as_optional<int>();
-  std::vector<int> sums;
-  int local = 0;
-  std::ranges::for_each(data, [&local, &sums](const auto &value) {
-    if (!value && local != 0) {
-      sums.push_back(local);
-      local = 0;
-      return;
-    }
-    local += *value;
-  });
-  if (local) {
-    sums.push_back(local);
-  }
+int task_1(const std::vector<int> &sums) { return std::ranges::max(sums); }
+
+int task_2(std::vector<int> &sums) {
   std::ranges::sort(sums);
   return std::accumulate(sums.end() - 3, sums.end(), 0);
 }
